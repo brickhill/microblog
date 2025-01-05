26b9d0591daa98b9b624421e8ba6afc0cdaec4f0
brickhill <petergibson@sbsys.co.uk>
2025-01-05 10:13:28 +0000

    Chapter 2

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,15 @@
 # 04.01.2025
 
 from app import app
-
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Nob off"
+    user = {"username": "Peter"}
+    posts = [
+        {"author": {"username": "Peter"}, "body": "Post number 1"},
+        {"author": {"username": "Frank"}, "body": "Post number 2"}
+    ]
+    return render_template("index.html", title=None, user=user, posts=posts)
+
