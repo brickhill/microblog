a3b30cacd9efcadb66edafac3f995a20e7404269
brickhill <petergibson@sbsys.co.uk>
2025-01-05 22:06:04 +0000

    Database migrations

--- config.py
+++ config.py
@@ -2,6 +2,9 @@
 # 05.01.2025
 
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "None of your business!"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
--- microblog.py
+++ microblog.py
@@ -1,4 +1,11 @@
 # Flask App startup transactio.
 # 04.01.2025
 
-from app import app
+from app import app, db
+from app.models import User, Post
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
