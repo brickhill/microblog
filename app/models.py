a4327aefb0797a9b844028862f288c2a26e58f4b
brickhill <petergibson@sbsys.co.uk>
2025-01-09 19:40:16 +0000

    Initial registration form implemented

--- app/models.py
+++ app/models.py
@@ -2,8 +2,11 @@ from typing import Optional
 from datetime import datetime, timezone
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
-class User(db.Model):
+from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -12,6 +15,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -25,3 +34,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+# The following is needed for flask_login
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
--- app/routes.py
+++ app/routes.py
@@ -2,24 +2,60 @@
 # 04.01.2025
 
 from app import app
-from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, login_required, logout_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from urllib.parse import urlsplit
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
-    user = {"username": "Peter"}
     posts = [
         {"author": {"username": "Peter"}, "body": "Post number 1"},
         {"author": {"username": "Frank"}, "body": "Post number 2"}
     ]
-    return render_template("index.html", title=None, user=user, posts=posts)
+    return render_template("index.html", title=None, posts=posts)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('login requested for user {} remember_me ={}'.format(form.username.data,
-        form.remember_me.data))
+        user = db.session.scalar(sa.select(User).where(
+            User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash('Logged out')
+    return redirect(url_for('index'))
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-    return render_template("login.html", title="Login", form=form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        print('a')
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulation.  You are now enrolled')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register ffs', form=form)
