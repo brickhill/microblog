63b1b280f345aed6158a53c939ac428725676bd0
brickhill <petergibson@sbsys.co.uk>
2025-01-12 12:33:02 +0000

    Error handling complete

--- app/forms.py
+++ app/forms.py
@@ -16,6 +16,18 @@ class EditProfileForm(FlaskForm):
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('submit')
 
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(
+                User.username == self.username.data
+            ))
+            if user is not None:
+                raise ValidationError("Please enter a different Username")
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -37,6 +49,7 @@ class RegistrationForm(FlaskForm):
             User.username == username.data))
         if user is not None:
             raise ValidationError('Please use a different username.')
+
 
     def validate_email(self, email):
         user = db.session.scalar(sa.select(User).where(
